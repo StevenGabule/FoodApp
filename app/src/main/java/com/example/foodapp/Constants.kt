66a833c7f9c3f2de410a274f74cfae34329f24d5
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-08-04 22:55:53 +0800

    initial the api

---
+++ app/src/main/java/com/example/foodapp/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foodapp
+
+class Constants {
+    companion object {
+        const val API_KEY = "8758d731714f4ea5919237449b0e560c"
+        const val BASE_URL = "https://api.spooncular.com"
+    }
+}
